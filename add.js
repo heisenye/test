a4be482b9c75c6490614bdd0d929d12f077cd197
heisenye <heisenye@163.com>
2025-07-04 13:31:49 +0800

    test(math): add unit tests for add function

--- add.js
+++ add.js
@@ -1,3 +1,11 @@
+import { test } from "node:test"
+import assert from 'assert'
+
 function add(a, b) {
     return a + b
 }
+
+test('add function', () => {
+    assert.strictEqual(add(1, 2), 3);
+    assert.strictEqual(add(-1, 1), 0);
+})
